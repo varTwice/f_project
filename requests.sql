460338de640d790e8da191e1650a0e17c09593d3
varTwice <43704683+varTwice@users.noreply.github.com>
2023-07-21 23:06:20 +0500

    Fix the first sql query

--- requests.sql
+++ requests.sql
@@ -1,7 +1,14 @@
-Задание 1
-SELECT c."login",COUNT(*)
+Задание 1 Выведи список логинов курьеров с количеством их заказов в статусе «В доставке» (поле inDelivery = true). 
+SELECT c."login",COUNT(k."id")
 FROM "Couriers" AS c 
-LEFT JOIN "Orders" AS k ON c."id" = k."courierId" and k."inDelivery" = true
+JOIN "Orders" AS k ON c."id" = k."courierId"
+WHERE k."inDelivery" = true and k."cancelled" = false and k."finished" = false
+GROUP BY c."login";
+
+-- для вывода всех логинов курьеров с количеством заказов
+SELECT c."login",COUNT(k."id")
+FROM "Couriers" AS c
+LEFT JOIN "Orders" AS k ON c."id" = k."courierId" and k."inDelivery" = true and k."cancelled" = false and k."finished" = false
 GROUP BY c."login";
 
 Задание 2
